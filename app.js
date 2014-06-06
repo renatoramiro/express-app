14c9e079a88aaca19b41ad6b15b4cfca7ee6b306
SES PB <ses@mail.com>
2014-06-06 16:48:47 -0300

    Adicionado pasta views

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var http = require('http');
-var express = require('express');
-var app = express();
+var http = require('http')
+	, express = require('express')
+	, app = express();
 
 http.createServer(app).listen(3000, function () {
 	console.log('Express app started.');
 });
 
 app.get('/', function (req, res) {
-	res.send('Welcome!');
+	res.send('<h1>Welcome!</h1>');
 });
