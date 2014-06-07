9e7a5ab38c3af35c27c5c7314d4ee4600cd4f931
Renato Ramiro <renato.ipj@gmail.com>
2014-06-06 21:47:24 -0300

    Adicionado rota e conteudo para as views

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ var http = require('http')
 	, express = require('express')
 	, app = express();
 
-http.createServer(app).listen(3000, function () {
-	console.log('Express app started.');
-});
+app.set('view engine', 'jade');
+app.set('views', './views');
 
 app.get('/', function (req, res) {
-	res.send('<h1>Welcome!</h1>');
+	res.render('index');
+});
+
+app.get('/say-hello', function (req, res) {
+	res.render('hello');
+});
+
+app.get('/test', function (req, res) {
+	res.send('<h1>This is a test</h1>');
+});
+
+http.createServer(app).listen(3000, function () {
+	console.log('Express app started.');
 });
