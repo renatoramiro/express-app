59016088a335e34ee7c2e8635d2d8cee7e56c84c
Renato Ramiro <renato.ipj@gmail.com>
2014-06-14 16:13:52 -0300

    Introdução a namespace

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var http = require('http')
-	, express = require('express')
-	, app = express();
+	, express = require('express');
+
+var namespace = require('express-namespace');
+var app = express();
 
 var iniparser = require('iniparser');
 var config = iniparser.parseSync('./config.ini');
@@ -14,6 +16,38 @@ app.use(express.responseTime());
 app.use(app.router);
 
 var routes = require('./routes')(app);
+
+app.namespace('/articles', function () {
+	app.get('/', function (req, res) {
+		res.send('Index of articles');
+	});
+
+	app.get('/new', function (req, res) {
+		res.send('New article');
+	});
+
+	app.get('/edit/:id', function (req, res) {
+		res.send('Edit article ' + req.params.id);
+	});
+
+	app.get('/delete/:id', function (req, res) {
+		res.send('Delete article ' + req.params.id);
+	});
+
+	app.get('/2013', function (req, res) {
+		res.send('Article from 2013');
+	});
+
+	app.namespace('/2013/jan', function () {
+		app.get('/', function (req, res) {
+			res.send('Articles from Jan 2013');
+		});
+
+		app.get('/nodejs', function(req, res) {
+			res.send('Articles about Node from jan 2013');
+		});
+	});
+});
 
 http.createServer(app).listen(config.port, function () {
 	console.log('Express app started on port ' + config.port);
